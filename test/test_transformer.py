ff925918c562394220bc5e6b2c03fb097156352e
Yamada <ymd_h@outlook.jp>
2023-05-04 20:00:45 +0900

    Fix: Update test

--- test/test_transformer.py
+++ test/test_transformer.py
@@ -17,22 +17,40 @@ from tsax.transformer import (
 
 class TestEmbedding(unittest.TestCase):
     def test_no_error(self):
-        key = jax.random.PRNGKey(0)
-        text = jnp.zeros((4, 12), dtype=int)
+        B = 4
+        L = 12
+        V = 5
+        dm = 3
 
-        e = Embedding(V=5, L=12)
+        key = jax.random.PRNGKey(0)
+        text = jnp.zeros((B, L), dtype=int)
+
+        e = Embedding(V=V, L=L, dm=dm)
         params = e.init(key, text)
 
-        ret = e.apply(params, text)
+        emb = e.apply(params, text)
+        self.assertEqual(emb.shape, (B, L, dm))
+
+        v = e.apply(params, emb, method=e.attend)
+        self.assertEqual(v.shape, (B, L, V))
 
     def test_no_error_jit(self):
-        key = jax.random.PRNGKey(0)
-        text = jnp.zeros((4, 12), dtype=int)
+        B = 4
+        L = 12
+        V = 5
+        dm = 3
 
-        e = Embedding(V=5, L=12)
+        key = jax.random.PRNGKey(0)
+        text = jnp.zeros((B, L), dtype=int)
+
+        e = Embedding(V=V, L=L, dm=dm)
         params = e.init(key, text)
 
-        ret = jax.jit(e.apply)(params, text)
+        emb = jax.jit(e.apply)(params, text)
+        self.assertEqual(emb.shape, (B, L, dm))
+
+        v = jax.jit(e.apply, static_argnames="method")(params, emb, method=e.attend)
+        self.assertEqual(v.shape, (B, L, V))
 
 
 if __name__ == "__main__":
