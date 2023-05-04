cde7e53facd9083b62facf954c0f678441e5ab7c
Yamada <ymd_h@outlook.jp>
2023-05-04 17:14:10 +0900

    Test for Embedding with jit

--- test/test_transformer.py
+++ test/test_transformer.py
@@ -25,6 +25,15 @@ class TestEmbedding(unittest.TestCase):
 
         ret = e.apply(params, text)
 
+    def test_no_error_jit(self):
+        key = jax.random.PRNGKey(0)
+        text = jnp.zeros((4, 12), dtype=int)
+
+        e = Embedding(V=5, L=12)
+        params = e.init(key, text)
+
+        ret = jax.jit(e.apply)(params, text)
+
 
 if __name__ == "__main__":
     unittest.main()
