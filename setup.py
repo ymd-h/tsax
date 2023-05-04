315d2e7e06e4892be4fcfe9ec3540b4407d6783c
Yamada <ymd_h@outlook.jp>
2023-05-04 16:18:34 +0900

    Fix install_requires

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup, find_packages
 
 setup(name="tsax",
       version="0.0.0",
-      requires = ["jax", "flax"],
+      install_requires = ["jax", "flax"],
       packages=find_packages())
