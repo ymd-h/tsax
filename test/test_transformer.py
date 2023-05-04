bde2e41e274f5db131625962cdf249fe8653ac35
Yamada <ymd_h@outlook.jp>
2023-05-04 16:41:33 +0900

    Test for Embedding

---
+++ test/test_transformer.py
@@ -0,0 +1,30 @@
+import unittest
+
+import jax
+import jax.numpy as jnp
+
+from tsax.transformer import (
+    Transformer,
+    Embedding,
+    EncoderStack,
+    EncoderLayer,
+    DecoderStack,
+    DecoderLayer,
+    MultiHeadAttention,
+    Attention,
+    FeedForward,
+)
+
+class TestEmbedding(unittest.TestCase):
+    def test_no_error(self):
+        key = jax.random.PRNGKey(0)
+        text = jnp.zeros((4, 12), dtype=int)
+
+        e = Embedding(V=5, L=12)
+        params = e.init(key, text)
+
+        ret = e.apply(params, text)
+
+
+if __name__ == "__main__":
+    unittest.main()
