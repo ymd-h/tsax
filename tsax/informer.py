7c2cdc367e147a4daef1b550d4fc196547fee832
Yamada <ymd_h@outlook.jp>
2023-05-13 09:33:02 +0900

    Start Informer

---
+++ tsax/informer.py
@@ -0,0 +1,217 @@
+"""
+Informer (:mod:`tsax.informer`)
+===============================
+
+Notes
+-----
+This module implements Informer [1]_.
+
+References
+----------
+.. [1] H. Zhou et al., "Informer: Beyond Efficient Transformer for Long Sequence Time-Series Forecasting", AAAI 2021, Vol. 35, No. 12
+   https://ojs.aaai.org/index.php/AAAI/article/view/17325,
+   https://arxiv.org/abs/2012.07436
+"""
+from __future__ import annotations
+import math
+
+import jax
+import jax.numpy as jnp
+from jax import Array
+from jax.random import KeyArray
+from jax.typing import ArrayLike
+from flax import linen as nn
+
+from .core import ResidualLayerNorm
+
+__all__ = [
+    "Informer",
+    "EncoderStack",
+    "DecoderStack",
+    "EncoderLayer",
+    "DecoderLayer",
+    "Distlling",
+    "MultiHeadAttention",
+    "ProbSparseAttention",
+    "FeedForward",
+]
+
+
+KERNEL_SIZE: int = 3
+"""
+Default Kernel Size for Conv1d at Distilling Layer
+"""
+
+
+class Distilling(nn.Module):
+    """
+    Distilling Layer
+    """
+    kernel: int = KERNEL_SIZE
+
+    @nn.compact
+    def __call__(self, x: ArrayLike) -> Array:
+        """
+        Call Distilling Layer
+
+        Parameters
+        ----------
+        x : ArrayLike
+            Inputs Sequence. [B, L, d]
+
+        Returns
+        -------
+        x : Array
+            Convoluted Sequence. [B, L/2, d]
+        """
+        x = nn.Conv(features=1, kernel_size=self.kernel)(x)
+        x = nn.activation.elu(x)
+        x = nn.max_pool(x, window_shape=(2,), strides=(2,))
+
+        return x
+
+
+
+class ProbSparseAttention(nn.Module):
+    """
+    ProbSparse self-attention
+    """
+    c: int
+
+    @nn.compact
+    def __call__(self,
+                 Q: ArrayLike,
+                 K: ArrayLike,
+                 V: ArrayLike,
+                 rng: KeyArray) -> Array:
+        """
+        Call ProbSparse self-attention
+
+        Parameters
+        ----------
+        Q : ArrayLike
+            Query. [B, L, d]
+        K : ArrayLike
+            Key. [B, L, d]
+        V : ArrayLike
+            Value. [B, L, d]
+        rng : KeyArray
+            Random Number Generator Key will be consumed.
+
+        Returns
+        -------
+        A : Array
+            ProbSparse self-attention
+        """
+        assert Q.shape[0] == K.shape[0] == V.shape[0], "BUG"
+        assert Q.shape[2] == K.shape[2] == V.shape[2], "BUG"
+
+        B: int = int(Q.shape[0])
+        m: int = int(Q.shape[1])
+        n: int = int(K.shape[1])
+        d: int = int(Q.shape[2])
+
+        u: int = int(self.c * math.log(m))
+        U: int = int(     m * math.log(n))
+
+
+        @jax.vmap
+        def _each_sample(_Q, _K, _V, _rng):
+            _Kbar = _K.at[jax.random.choice(_rng, U, replace=False),:].get()
+
+            _Sbar = jnp.matmul(_Q, jnp.transpose(_Kbar, (1, 0)))
+            _M = jnp.max(_Sbar, axis=1) - jnp.mean(_Sbar, axis=1)
+            assert _M.shape == (m, U), "BUG"
+
+            _, _I = jax.lax.top_k(_M, u)
+            assert _I.shape == (u,), "BUG"
+
+            _Qbar = _Q.at[_I, :].get()
+            assert _Qbar.shape == (u, d), "BUG"
+
+            _QbarK = jnp.matmul(_Qbar, jnp.transpose(_K, (1, 0)))
+            assert _QbarK.shape == (u, n), "BUG"
+
+            _S1 = jnp.matmul(nn.activation.softmax(_QbarK / math.sqrt(d)), _V)
+            assert _S1.shape == (u, d), "BUG"
+
+            _S = jnp.zeros((m, d), dtype=Q.dtype)
+            _S = _S.at[:, :].set(jnp.mean(_V, axis=1, keepdims=True))
+            _S = _S.at[_I, :].set(_S1)
+
+            return _S
+
+        rng_batch = jax.random.split(rng, B)
+
+        S = _each_sample(Q, K, V, rng_batch)
+        assert S.shape == (B, m, d), "BUG"
+
+        return S
+
+
+class MultiHeadAttention(nn.Module):
+    """
+    Multi Head Attention Layer
+
+    Attributes
+    ----------
+    c : int
+        Hyper Parameter
+    nH : int
+        Number of Multi Head
+    dm : int
+        Model Dimension
+    """
+    c: int
+    nH: int = NH
+    dm: int = DM
+    Pdrop: float = PDROP
+
+    @nn.compact
+    def __call__(self,
+                 Q: ArrayLike,
+                 K: ArrayLike,
+                 V: ArrayLike,
+                 rng: KeyArray, *,
+                 with_dropout: bool = False) -> Array:
+        """
+        Call Multi Head Attention
+
+        Parameters
+        ----------
+        Q : ArrayLike
+            Query. [B, L, dm]
+        K : ArrayLike
+            Key. [B, L, dm]
+        V : ArrayLike
+            Value. [B, L, dm]
+        rng : KeyArray
+            Random Number Generator Key will be consumed.
+        with_dropout : bool, optional
+            Whether dropout or not
+
+        Returns
+        -------
+        MHA : Array
+            Multi Head Attention. [B, L, dm]
+        """
+        assert K.shape == V.shape, "BUG"
+        assert Q.shape[0] == K.shape[0], "BUG"
+        assert Q.shape[2] == K.shape[2], "BUG"
+
+        rngs = jax.random.split(rng, self.nH)
+
+        # x: [B, L, dm (= dm/nH * nH)]
+        d: int = self.dm // self.nH
+        x = jnp.concatenate([ProbSparseAttention(name=f"head_{i}")(Q, K, V, rngs[i])
+                             for i in range(self.nH)],
+                            axis=2)
+        assert x.shape == (*Q.shape[:2], d * self.nH), "BUG"
+
+        MHA = nn.Dense(features=self.dm, use_bias=Fase, name="WO")(x)
+        assert Q.shape == MHA.shape, "BUG"
+
+        if with_dropout:
+            MHA = MHA.at[:].set(nn.Dropout(self.Pdrop, deterministic=False)(MHA))
+
+        return MHA
