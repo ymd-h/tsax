27afbdfda6bdf0eaf6bb00be9985cbdcdb3204e9
Yamada <ymd_h@outlook.jp>
2023-05-14 22:34:00 +0900

    Test Multi Dimension for ConvSeq

--- test/test_convolution.py
+++ test/test_convolution.py
@@ -27,6 +27,26 @@ class TestConvolution(TestCase):
 
         self.assertAllclose(x_conv, x_conv_jit)
 
+    def test_multi_conv(self):
+        B, L, d, dm = 1, 3, 5, 3
+
+        key = jax.random.PRNGKey(0)
+
+        key, key_use = jax.random.split(key, 2)
+        x = jax.random.normal(key_use, (B, L, d))
+
+        conv = ConvSeq(dm=dm)
+
+        key, key_use = jax.random.split(key, 2)
+        x_conv, _ = conv.init_with_output(key_use, x)
+        self.assertEqual(x_conv.shape, (B, L, dm))
+
+        x_conv_jit, _ = jax.jit(conv.init_with_output)(key_use, x)
+        self.assertEqual(x_conv_jit.shape, (B, L, dm))
+
+        self.assertAllclose(x_conv, x_conv_jit)
+
+
 
 if __name__ == "__main__":
     unittest.main()
