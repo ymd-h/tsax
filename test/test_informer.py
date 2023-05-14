a24195d804513354fc94586a8b8d651efbf36a57
Yamada <ymd_h@outlook.jp>
2023-05-14 22:44:42 +0900

    Test categorical input for Informer

--- test/test_informer.py
+++ test/test_informer.py
@@ -39,6 +39,30 @@ class TestEmbedding(TestCase):
 
         self.assertAllclose(embedded, embedded_jit)
 
+    def test_with_categorical(self):
+        B, L, d, Vs, dm = 1, 5, 2, (7, 12), 3
+
+        key = jax.random.PRNGKey(0)
+
+        key, key_use = jax.random.split(key, 2)
+        x = jax.random.normal(key_use, (B, L, d))
+
+        key, key_use = jax.random.split(key, 2)
+        c = jax.random.randint(key_use,
+                               (B, L, len(Vs)),
+                               0, jnp.asarray(Vs, dtype=int))
+
+        e = Embedding(dm=dm, Vs=Vs)
+
+        key, key_use = jax.random.split(key, 2)
+        embedded, _ = e.init_with_output(key_use, x, c)
+        self.assertEqual(embedded.shape, (B, L, dm))
+
+        embedded_jit, _ = jax.jit(e.init_with_output)(key_use, x, c)
+        self.assertEqual(embedded_jit.shape, (B, L, dm))
+
+        self.assertAllclose(embedded, embedded_jit)
+
 
 
 if __name__ == "__main__":
