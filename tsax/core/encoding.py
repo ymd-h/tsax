9eeda9bc86cf1962c38ffd8b7c11e1a9a432e650
Yamada <ymd_h@outlook.jp>
2023-05-14 11:53:37 +0900

    Update PositionalEncoding

--- tsax/core/encoding.py
+++ tsax/core/encoding.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from typing import Optional
 
 import jax
 import jax.numpy as jnp
@@ -12,31 +13,85 @@ __all__ = [
 ]
 
 
-class PositionalEncoding(nn.Module):
-    r"""
-    Positional Encoding
+def positional_encoding(dm: int,
+                        L: int,
+                        Lfreq: int, *,
+                        dtype: Optional[jnp.dtype] = None) -> Array:
+    """
+    Create Positional Encoding
 
-    Attributes
+    Parameters
     ----------
     dm : int
         Model Dimension
     L : int
-        Length
+        Input Sequence Length
+    Lfreq : int
+        Frequency Normalization Length.
+    dtype : jax.numpy.dtype, optional
+        Data Type.
+
+    Returns
+    -------
+    PE : Array
+        Positional Encoding. [L, dm]
+    """
+    half: int = dm // 2
+    sin_dim: int = half + (dm % 2)
+    cos_dim: int = half
+
+    freq = 1.0 / (Lfreq ** (2 * jnp.arange(sin_dim) / dm))
+    theta = jax.vmap(lambda pos: pos * freq)(jnp.arange(L))
+
+    PE = (jnp.zeros((L, dm), dtype=dtype)
+          .at[:,0::2].set(jnp.sin(theta))
+          .at[:,1::2].set(jnp.cos(theta.at[:,:cos_dim].get())))
+
+    return PE
+
+
+class PositionalEncoding:
+    r"""
+    Positional Encoding
 
     Notes
     -----
-    ``PE(pos, 2j  ) = sin(pos / (L ** (2 * j / dm)))``
-    ``PE(pos, 2j+1) = cos(pos / (L ** (2 * j / dm)))``
+    ``PE(pos, 2j  ) = sin(pos / (Lfreq ** (2 * j / dm)))``
+    ``PE(pos, 2j+1) = cos(pos / (Lfreq ** (2 * j / dm)))``
     """
-    dm: int
-    L: int
 
-    def setup(self) -> None:
-        half: int = self.dm // 2
-        self.sin_dim: int = half + (self.dm % 2)
-        self.cos_dim: int = half
+    def __init__(self,
+                 dm: int,
+                 L: int, *,
+                 Lfreq: int = 10000,
+                 lazy: bool = False,
+                 dtype: Optional[jnp.dtype] = None):
+        """
+        Initialize Positional Encoding
 
-        self.freq = 1.0 / (self.L ** (2 * jnp.arange(self.sin_dim) / self.dm))
+        Parameters
+        ----------
+        dm : int
+            Model Dimension
+        L : int
+            Input Sequence Length
+        Lfreq : int, optional
+            Frequency Normalization Length. Default is ``10000``.
+        lazy : bool, optional
+            If ``False`` (default), prepare beforehand.
+        dtype : jax.numpy.dtype, optional
+            Data Type.
+        """
+        self.dm: int = dm
+        self.L: int = L
+        self.Lfreq = Lfreq
+        self.lazy: bool = lazy
+
+        if not self.lazy:
+            self.pe = positional_encoding(dm=self.dm,
+                                          L=self.L,
+                                          Lfreq=self.Lfreq,
+                                          dtype=dtype)
 
     def __call__(self, x: ArrayLike) -> Array:
         """
@@ -45,22 +100,19 @@ class PositionalEncoding(nn.Module):
         Parameters
         ----------
         x : ArrayLike
-            Inputs. [B, Lx, dm]
+            Inputs. [B, L, dm]
 
         Returns
         -------
         PE : Array
-            Positional Encoding. [Lx, dm]
+            Positional Encoding. [L, dm]
         """
-        assert x.shape[2] == self.dm, "BUG"
+        assert x.shape[1:] == (self.L, self.dm), "BUG"
 
-        Lx: int = x.shape[1]
-        theta = jax.vmap(lambda pos: pos * self.freq)(jnp.arange(Lx))
+        if self.lazy:
+            return positional_encoding(dm=self.dm,
+                                       L=self.L,
+                                       Lfreq=self.Lfreq,
+                                       dtype=x.dtype)
 
-        PE = jnp.zeros((Lx, self.dim), dtype=x.dtype)
-
-        PE = (PE
-              .at[:,0::2].set(jnp.sin(theta))
-              .at[:,1::2].set(jnp.cos(theta.at[:,:self.cos_dim].get())))
-
-        return PE
+        return self.pe
