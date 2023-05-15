64176f6284bb257315ef304a5794e2ca49942181
Yamada <ymd_h@outlook.jp>
2023-05-15 22:30:50 +0900

    Add logging

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup, find_packages
 
 setup(name="tsax",
       version="0.0.0",
-      install_requires = ["jax", "flax"],
+      install_requires = ["jax", "flax", "well-behaved-logging"],
       packages=find_packages())
---
+++ tsax/logging.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+from logging import getLogger, Handler
+from typing import Iterable, Union
+
+import wblog
+
+
+def enable_logging(level: Optional[int] = None,
+                   handlers:  Union[Handler, Iterable[Handler], None] = None,
+                   propagate: bool = True) -> None:
+    """
+    Enable Logging for TSax
+
+    Parameters
+    ----------
+    level : int, optional
+        Logging Level
+    handlers : logging.Handler, iterable of logging.Handler, optional
+        Handlers
+    propagate : bool, optional
+        Whether propagate to parent logger
+    """
+    wblog.start_logging("tsax", level, handlers=handlers, propagate=propagate)
+
+
+def disable_logging() -> None:
+    """
+    Disable Logging for TSax
+    """
+    wblog.stop_logging("tsax")
