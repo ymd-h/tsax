041a482796e10c47cad2b44f0e9499acf42420b4
Yamada <ymd_h@outlook.jp>
2023-05-16 20:50:14 +0900

    WIP: Update train()

--- tsax/experiment.py
+++ tsax/experiment.py
@@ -37,12 +37,44 @@ def train(model,
     batch_size : int
         Batch Size
     """
+    t0 = time.perf_counter()
+
     rngkey, key_use = jax.random.split(rngkey, 2)
-    params = model.init(key_use,
-                        )
+    params = model.init(key_use, ...)
+    opt_sate = optimizer.init(params)
 
+    idx = jnp.arange(train_size)
+    nbatch: int = train_size // batch_size
     logger.info("Start Training")
+    for ep in range(epoch):
+        t = time.perf_counter()
+        rngkey, key_use = jax.random.split(rngkey, 2)
+        idx = jax.random.permutation(key_use, idx)
 
+        epoch_loss: float = 0
+        for i in tqdm(range(nbatch), ascii=True, leave=False):
+            bidx = idx.at[i:i+batch_size].get()
+            rngkey, key_use = jax.random.split(rngkey, 2)
+            loss, grad = train_fn(params,
+                                  key_use, ...)
+
+            epoch_loss += loss
+            update, opt_state = optimizer.update(grad, opt_state)
+            params = optax.apply_updates(params, update)
+
+        dt = (time.perf_counter() - t)
+        logger.info("Train: Epoch %d, Loss: %.6f, Elapssed: %.3f sec",
+                    ep, epoch_loss / (batch_size * nbatch), dt)
+
+        if ep % valid_freq == 0:
+            valid_loss = loss_fn(params, None, ...)
+            logger.info("Valid: Epoch: %d, Loss: %.6f",
+                        ep, valid_loss / valid_size)
+
+
+    final_loss = loss_fn(params, None, ...)
+    logger.info("Final Valid: Total Epoch %d, Loss: %.6f, Elapsed: %.3f sec",
+                epoch, final_loss / valid_size, time.perf_counter() - t0)
 
 
 def inference(model, rngkey: KeyArray) -> Array:
