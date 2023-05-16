24ff9309cefde4b1dcb0c4901bf48b5e17ed829a
Yamada <ymd_h@outlook.jp>
2023-05-16 20:06:08 +0900

    Update setup

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(name="tsax",
+      description="TSax: Time Series Forecasting",
       version="0.0.0",
       install_requires = ["jax", "flax", "well-behaved-logging"],
+      extras_require={
+          "experiment": ["optax", "tqdm"],
+      },
       packages=find_packages())
