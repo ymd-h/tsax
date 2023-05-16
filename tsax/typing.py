e29fb9a2e896f568fa937f6105b0a5af19f0fabc
Yamada <ymd_h@outlook.jp>
2023-05-16 12:25:00 +0900

    Add tsax.typing

---
+++ tsax/typing.py
@@ -0,0 +1,15 @@
+"""
+Tsax Typing (:mod:`tsax.typing`)
+================================
+"""
+from __future__ import annotations
+
+from jax import Array
+from jax.typing import ArrayLike
+from jax.random import KeyArray
+
+__all__ = [
+    "Array",
+    "ArrayLike",
+    "KeyArray",
+]
