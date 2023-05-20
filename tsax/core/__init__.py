943d99c5d158adab652724dc6fd280044b5ab57b
Yamada <ymd_h@outlook.jp>
2023-05-20 21:23:00 +0900

    Export SubsequentMask

--- tsax/core/__init__.py
+++ tsax/core/__init__.py
@@ -5,3 +5,4 @@ Core (mod:`tsax.core`)
 from .normalization import ResidualLayerNorm
 from .encoding import PositionalEncoding, CategoricalEncoding
 from .convolution import ConvSeq
+from .mask import SubsequentMask
