3f6e276edf573ae56d6a3d1dd39bf9a5a92f463c
Yamada <ymd_h@outlook.jp>
2023-05-20 21:05:01 +0900

    Add SubsequentMask

---
+++ tsax/core/mask.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+import jax
+import jax.numpy as jnp
+
+from tsax.typing import Array
+
+
+def SubsequentMask(L: int) -> Array:
+    """
+    Generate Subsequent Mask for Attention
+
+    Parameters
+    ----------
+    L : int
+       Length of Sequence
+
+    Returns
+    -------
+    mask : Array
+        Subsequent Mask. [L, L]
+    """
+    return jnp.tril(jnp.ones(L, L), dtype=int)
