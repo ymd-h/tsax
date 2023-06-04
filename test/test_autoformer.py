71a3f5e3fcfe95ffe49fdd7a2163b994476b1e21
Yamada <ymd_h@outlook.jp>
2023-06-04 21:57:46 +0900

    Test for SeriesDecomp

--- test/test_autoformer.py
+++ test/test_autoformer.py
@@ -3,6 +3,7 @@ import unittest
 import jax
 import jax.numpy as jnp
 
+from tsax.testing import TestCase
 from tsax.model.autoformer import (
     Autoformer,
     EncoderStack,
@@ -14,6 +15,16 @@ from tsax.model.autoformer import (
     SeriesDecomp,
 )
 
+
+class TestSeriesDecomp(TestCase):
+    def test_sin(self):
+        x = jnp.reshape(jnp.sin(0.5 * jnp.arange(12) * jnp.pi), (1, -1, 1))
+        (s, t), _ = SeriesDecomp(kMA=4).init_with_output(jax.random.PRNGKey(0), x)
+
+        self.assertEqual(x.shape, s.shape)
+        self.assertEqual(x.shape, t.shape)
+        self.assertAllclose(t, 0, atol=1e-6, rtol=1e-6)
+        self.assertAllclose(s, x, atol=1e-6, rtol=1e-6)
 
 
 
