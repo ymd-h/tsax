d4036aa892387a7a7025fe03abca995d7148a502
Yamada <ymd_h@outlook.jp>
2023-06-11 16:33:38 +0900

    Add get_version_tuple

--- tsax/version.py
+++ tsax/version.py
@@ -4,20 +4,45 @@ Version module (:mod:`tsax.version`)
 """
 from __future__ import annotations
 from importlib.metadata import version
+from typing import Tuple
 
 __all__ = [
     "get_version",
+    "get_version_tuple",
 ]
 
 
 
-def get_version() -> str:
+def get_version(pkg: str = "tsax") -> str:
     """
-    Get TSax Version
+    Get Version
+
+    Parameters
+    ----------
+    pkg : str, optional
+        Package Name. Default is ``"tsax"``
 
     Returns
     -------
     version : str
         Verion String
     """
-    return version("tsax")
+    return version(pkg)
+
+
+def get_version_tuple(pkg: str = "tsax") -> Tuple[int, int, int]:
+    """
+    Get Version as tuple
+
+    Parameters
+    ----------
+    pkg : str, optional
+        Package Name. Default is ``"tsax"``
+
+    Returns
+    -------
+    version : tuple of ints
+        Version tuple
+    """
+    return tuple(int(v) for v in get_version(pkg))
+
