437e45cffd67f413a34281f053d84ef0e574d07c
Yamada <ymd_h@outlook.jp>
2023-06-12 22:51:59 +0900

    Add MetricsSummary adding

--- tsax/stat.py
+++ tsax/stat.py
@@ -34,9 +34,47 @@ class MetricsSummary(PyTreeNode):
         """
         assert runs.shape[0] > 1, "BUG"
 
+        N = runs.shape[0]
+        V = jnp.var(runs, axis=0, ddof=1)
         return MetricsSummary(
-            N=runs.shape[0],
+            N=N,
             mean=jnp.mean(runs, axis=0),
-            mean_error=jnp.std(runs, axis=0, ddof=1) / jnp.sqrt(runs.shape[0]),
-            unbiased_variance=jnp.var(runs, axis=0, ddof=1)
+            mean_error=jnp.sqrt(V/N),
+            unbiased_variance=V
+        )
+
+    def add(other: MetricsSummary) -> MetricsSummary:
+        """
+        Add 2 MetricsSummary and Returns New One
+
+        Parameters
+        ----------
+        other : MetricsSummary
+            Other metrics
+
+        Returns
+        -------
+        MetricsSummary
+            Newly created summed MetricsSummay
+        """
+        N = self.N + other.N
+        mean = (self.N * self.mean + other.N * other.mean) / N
+
+        def f(n, m, uv):
+            return n * (m**2 + ((n-1)/n * uv) ** 2)
+
+        # Ref: https://stats.stackexchange.com/a/43183
+        uV = (
+            (
+                f( self.N,  self.mean,  self.unbiased_variance) +
+                f(other.N, other.mean, other.unbiased_variance)
+            ) / N
+            - (mean ** 2)
+        ) * (N/(N-1))
+
+        return MetricsSummary(
+            N=N,
+            mean=mean,
+            mean_error=jnp.sqrt(uV/N),
+            unbiased_variance=uV,
         )
