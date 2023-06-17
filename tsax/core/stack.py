8db38635d61590031a6ea052d2e7018741db00f4
Yamada <ymd_h@outlook.jp>
2023-06-17 11:36:04 +0900

    Fix: type

--- tsax/core/stack.py
+++ tsax/core/stack.py
@@ -3,7 +3,7 @@ Layer Stack (:mod:`tsax.core.stack`)
 ====================================
 """
 from __future__ import annotations
-from typing import Callable, Optional, TypeVar, Tuple
+from typing import cast, Callable, Optional, TypeVar, Tuple
 
 import jax
 import flax.linen as nn
@@ -16,14 +16,13 @@ __all__ = [
     "LayerStack"
 ]
 
-C = TypeVar("C", bound=DataT)
 T = TypeVar("T")
+Layer = TypeVar("Layer", bound=nn.Module)
 
-
-def LayerStack(layer: nn.Module,
-               x: C,
+def LayerStack(layer: Layer,
+               x: DataT,
                n: int,
-               f: Optional[Callable[[nn.Module, C], C]]=None) -> Array:
+               f: Optional[Callable[[Layer, DataT], DataT]]=None) -> DataT:
     """
     Call Identical Layer Stack
 
@@ -39,16 +38,21 @@ def LayerStack(layer: nn.Module,
         Conversion function when I/O signature of ``layer``
         is different from ``x``
 
+    Returns
+    -------
+    x : DataT
+        Data processed Stack of Layers
+
     Notes
     -----
     This function is a convenient wrapper of ``flax.linen.scan``.
     """
     if f is None:
-       def F(L: nn.Module, carry: C, _: None) -> Tuple[C, None]:
+       def F(L: Layer, carry: DataT, _: None) -> Tuple[DataT, None]:
            return L(carry), None
     else:
-        def F(L: nn.Module, carry: C, _: None) -> Tuple[C, None]:
-            return f(L, carry), None
+        def F(L: Layer, carry: DataT, _: None) -> Tuple[DataT, None]:
+            return cast(Callable[[Layer, DataT], DataT], f)(L, carry), None
 
     x, _ = nn.scan(
         F,
