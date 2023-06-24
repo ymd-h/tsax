8b179033da942ced9fdf073c1d6cdcf9b203cac2
Yamada <ymd_h@outlook.jp>
2023-06-24 15:09:42 +0900

    Fix: Import Literal

--- tsax/typing.py
+++ tsax/typing.py
@@ -3,7 +3,7 @@ Tsax Typing (:mod:`tsax.typing`)
 ================================
 """
 from __future__ import annotations
-from typing import Any, Dict, List, Optional, Tuple, TypeVar, Union
+from typing import Any, Dict, List, Literal, Optional, Tuple, TypeVar, Union
 
 from jax import Array
 from jax.typing import ArrayLike
