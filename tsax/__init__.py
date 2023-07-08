5c68140dd3a1176fba865209a33bce906c9ac8e7
Yamada <ymd_h@outlook.jp>
2023-07-08 10:36:39 +0900

    Remove deprecated Transformer

--- tsax/__init__.py
+++ tsax/__init__.py
@@ -19,6 +19,5 @@ TSax has 3 different level Usage.
   2. Predefined Experiment API. See `tsax.optional.experiment`
   3. Use only Predefined Models. See `tsax.model`
 """
-from .model.transformer import Transformer
 from .model.informer import Informer
 from .model.autoformer import Autoformer
